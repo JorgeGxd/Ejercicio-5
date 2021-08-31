ea47383961e29e690a0bfd6608d8bf6c24cf6d44
= <olstertecn597@gmail.com>
2021-08-31 11:55:44 -0600

    version 1.0

---
+++ src/ejercicio5/Student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio5;
+
+/**
+ *
+ * @author olste
+ */
+public class Student {
+    public String nombre;
+    public double promedio;
+    Student(String n, double p){
+        this.nombre = n;
+        this.promedio = p;     
+    }
+    
+}
